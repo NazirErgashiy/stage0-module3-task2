5f1b1be321519b77181f831c327d5c4fa4a8e156
Nazi <pikobiznes@gmail.com>
2022-09-07 18:03:20 +0500

    2

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
@@ -10,7 +10,7 @@ public class advancedNamingConvention {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void callByNumber(int number) {
         //some code
     }
 }
